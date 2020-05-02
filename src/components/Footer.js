365164dfaa2720b9255e46f98d65ec9a21c63c84
RG-404 <gogoi.rishparn@gmail.com>
2020-05-03 01:48:00 +0530

    Updated Readme.md

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -111,13 +111,13 @@ const Footer = class extends React.Component {
             </div>
             <div className="col-md-6 pointer creators">
               Website developed by &nbsp;
-                            <a href="https://www.linkedin.com/in/rishi-gogoi/" target="_blank" rel="noreferrer noopener">
+                            {/* <a href="https://www.linkedin.com/in/rishi-gogoi/" target="_blank" rel="noreferrer noopener"> */}
                 Rishparn Gogoi
-                            </a>
+                            {/* </a> */}
                              &nbsp;&amp;&nbsp;
-                            <a href="about:blank" target="_blank" rel="noreferrer noopener">
+                            {/* <a href="about:blank" target="_blank" rel="noreferrer noopener"> */}
                 Subhranan Bora
-                            </a>
+                            {/* </a> */}
             </div>
           </div>
         </div>
