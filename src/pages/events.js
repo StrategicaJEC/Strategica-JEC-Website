7fea68d2803b74ff7fadbeff19f2a0eb5e532388
RG-404 <gogoi.rishparn@gmail.com>
2020-06-02 15:42:33 +0530

    event updated

--- src/pages/events.js
+++ src/pages/events.js
@@ -1,48 +1,179 @@
-import React from 'react';
+import React from "react";
 
-import Layout from "../components/Layout"
-import SEO from "../components/seo"
+import Layout from "../components/Layout";
+import SEO from "../components/seo";
 
 import "./events.css";
 const About = () => {
-    return (
-        <Layout>
-        <SEO title="Events" description="Events organized by Strategica, Jorhat Engineering College"/>
-            <div className="events">
-                <div className="container-fluid banner">
-                    <h1 className="display-3">Events and Activities</h1>
-                </div>
-                <div className="container-fluid py-4 main animated fadeIn slow">
-                    <div className="timeline">
-                        <div className="time-container left">
-                            <div className="content">
-                                <h2>30 April 2020</h2>
-                                <p>
-                                    A moment of pride arrived for us as we got the opportunity to hear to retired IAS officer, <strong>Mr. Anil Swarup</strong>, former Coal and School Education Secretary of India on <strong>'Digital India:What COVID-19 has to offer in this regard'</strong> in our club's webinar session.
-                                </p>
-                            </div>
-                        </div>
-                        <div className="time-container right">
-                            <div className="content">
-                                <h2>22 April 2020</h2>
-                                <p>
-                                    On the occasion of World Earth Day, Strategica organised a webinar on <strong>'Sustainable Waste Management: How emerging technologies can help mitigate and respond to epidemic outbreak risks with special reference to COVID-19'</strong> by <strong>Mr. Moharana Choudhury</strong>. Our guest speaker has been a ex-research fellow of ISRO and a recipient of India's Great Leaders Award.
-                                </p>
-                            </div>
-                        </div>
-                        <div className="time-container left">
-                            <div className="content">
-                                <h2>Club members are doing a study:</h2>
-                                <ul>
-                                    <li>Impact analysis of COVID19 on Indian economy </li>
-                                    <li>Prediction analysis of Assam's agricultural sector post lockdown and Bordoisila, with a special focus on Assam floods and the resulting food inflation in the market.</li>
-                                </ul>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+  return (
+    <Layout>
+      <SEO
+        title="Events"
+        description="Events organized by Strategica, Jorhat Engineering College"
+      />
+      <div className="events">
+        <div className="container-fluid banner">
+          <h1 className="display-3">Events and Activities</h1>
+        </div>
+        <div className="container-fluid py-4 main animated fadeIn slow">
+          <div className="timeline">
+            <div className="time-container left">
+              <div className="content">
+                <h2>24 May 2020</h2>
+                <img
+                  src="https://res.cloudinary.com/dog2bkgwo/image/upload/v1591091786/events/Himanshu_Sikaria_tojpa5.jpg"
+                  alt="Himanshu Sikaria"
+                  title="Himanshu Sikaria"
+                />
+                <p className="mt-3">
+                  On 24th May 2020 Strategica in collaboration with Startup
+                  Cell, Jorhat Engineering College and powered by Career Spark,
+                  organised a webinar on{" "}
+                  <strong>'Students and Startups'</strong> by{" "}
+                  <strong>Himanshu Sikaria</strong>, TedX Speaker and founder of
+                  “Startify” and later acquired it while he was a student of IIT
+                  Jodhpur.
+                </p>
+              </div>
             </div>
-        </Layout>
-    )
-}
+            <div className="time-container right">
+              <div className="content">
+                <h2>15 May 2020</h2>
+                <img
+                  src="https://res.cloudinary.com/dog2bkgwo/image/upload/v1591091786/events/Vivek_Atray_sdj4we.jpg"
+                  alt="Vivek Atray"
+                  title="Vivek Atray"
+                />
+                <p className="mt-3">
+                  STRATEGICA club cum students community conducts yet another
+                  webinar on{" "}
+                  <strong>
+                    'Scenario in India after COVID-19, with special reference to
+                    tourism, and leadership qualities required for it'
+                  </strong>{" "}
+                  by <strong>Vivek Atray</strong> Our guest is an ex-IAS officer
+                  and a TED speaker. He is the author of three books, titled
+                  "Finding Success Within", "Move on Bunnyrand "Dubey Ji Bounces
+                  Back". It was an amazing session as it was enlightening how
+                  India's market size is humongous which holds a chance to
+                  upgrade by inviting investors from other countries. "Tourism
+                  has to be natural to a country", he says. Tourism sector can
+                  be a success if and only if perception, facilitation,
+                  security, sanitation, health is taken special care of. State
+                  government must take the responsibility of making the state
+                  exist in the eyes of the world. Apart from that the tourism
+                  website must be user friendly and responsive to any queries
+                  put to them. He says, "Leadership must be carried forth with
+                  emotional intelligence.", to which I agree completely. There
+                  must be a sense of empathy towards the people we are working
+                  with. Last but not the least, the 8 Cs of success he stated -
+                  Courage, Character, Creativity, Cheerfulnees, Compassion,
+                  Contentment, Calmness, Communication - which must be kept in
+                  mind in life and implemented.
+                </p>
+              </div>
+            </div>
+            <div className="time-container left">
+              <div className="content">
+                <h2>7 May 2020</h2>
+                <img
+                  src="https://res.cloudinary.com/dog2bkgwo/image/upload/v1591091786/events/Siddhartha_Sharma_yvselv.jpg"
+                  alt="Siddhartha Sharma"
+                  title="Siddhartha Sharma"
+                />
+                <p className="mt-3">
+                  Strategica Finance Group organised a interactive session on
+                  <strong>
+                    'Financial Markets in India and Career in Financial
+                    Analytics with Siddhartha Sharma
+                  </strong>
+                  , Founder @ CredFIC
+                </p>
+              </div>
+            </div>
+            <div className="time-container right">
+              <div className="content">
+                <h2>4 May 2020</h2>
+                <img
+                  src="https://res.cloudinary.com/dog2bkgwo/image/upload/v1591091787/events/Chiranjib_Bhowmick_ats3ti.jpg"
+                  alt="Chiranjib Bhowmick"
+                  title="Chiranjib Bhowmick"
+                />
+                <p className="mt-3">
+                  4th day of May and STRATEGICA hosts yet another awesome
+                  webinar on 'Research prospectives to combat COVID-19: An Al
+                  based approach'. Our guest speaker was
+                  <strong>Chiranjib Bhowmick</strong>, Prime Minister's Research
+                  Fellow, School of Medical Science & Technology, IIT Kharagpur.
+                  We got an overview of the research prospectives; Al based
+                  disease detection, biosensor based detection, personal
+                  protective equipments for health care workers, large scale
+                  sanitation,low cost ventilators and telemedicine applications.
+                  We also got a basic understanding of how deep learning
+                  algorithms can diagnose and monitor #covidl9 cases from lung
+                  images. With all these technologies India is sure to see a
+                  better future.
+                </p>
+              </div>
+            </div>
+            <div className="time-container left">
+              <div className="content">
+                <h2>30 April 2020</h2>
+                <img
+                  src="https://res.cloudinary.com/dog2bkgwo/image/upload/v1591091786/events/Anil_Swarup_huaosb.jpg"
+                  alt="Anil Swarup"
+                  title="Anil Swarup"
+                />
+                <p className="mt-3">
+                  A moment of pride arrived for us as we got the opportunity to
+                  hear to retired IAS officer, <strong>Mr. Anil Swarup</strong>,
+                  former Coal and School Education Secretary of India on{" "}
+                  <strong>
+                    'Digital India:What COVID-19 has to offer in this regard'
+                  </strong>{" "}
+                  in our club's webinar session.
+                </p>
+              </div>
+            </div>
+            <div className="time-container right">
+              <div className="content">
+                <h2>22 April 2020</h2>
+                <img
+                  src="https://res.cloudinary.com/dog2bkgwo/image/upload/v1591092629/events/Moharana_Choudhury_xptlpo.jpg"
+                  alt="Moharana Choudhury"
+                  title="Moharana Choudhury"
+                />
+                <p className="mt-3">
+                  On the occasion of World Earth Day, Strategica organised a
+                  webinar on{" "}
+                  <strong>
+                    'Sustainable Waste Management: How emerging technologies can
+                    help mitigate and respond to epidemic outbreak risks with
+                    special reference to COVID-19'
+                  </strong>{" "}
+                  by <strong>Mr. Moharana Choudhury</strong>. Our guest speaker
+                  has been a ex-research fellow of ISRO and a recipient of
+                  India's Great Leaders Award.
+                </p>
+              </div>
+            </div>
+            <div className="time-container left">
+              <div className="content">
+                <h2>Club members are doing a study:</h2>
+                <ul>
+                  <li>Impact analysis of COVID19 on Indian economy </li>
+                  <li>
+                    Prediction analysis of Assam's agricultural sector post
+                    lockdown and Bordoisila, with a special focus on Assam
+                    floods and the resulting food inflation in the market.
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </Layout>
+  );
+};
 export default About;
