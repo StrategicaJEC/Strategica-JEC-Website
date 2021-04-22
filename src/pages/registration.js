4607d751cd41e15dd2a18e39b7e69a76a5e7e732
RG-404 <55778353+RG-404@users.noreply.github.com>
2021-04-23 02:20:32 +0530

    add page registration

---
+++ src/pages/registration.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+import Layout from "../components/Layout";
+import SEO from "../components/seo";
+
+import "./registration.css";
+const Registration = () => {
+  return (
+    <Layout>
+      <div className="container-fluid registration">
+        <iframe
+          src="https://docs.google.com/forms/d/e/1FAIpQLSdOQc7dwBc-2enkiD0vYaqrsjE9dsQ0VJ-A-HkbBNvyyBk9Sw/viewform?embedded=true"
+          scrolling="no"
+          width="640"
+          height="1369"
+          frameborder="0"
+          marginheight="0"
+          marginwidth="0"
+        >
+          Loading…
+        </iframe>
+      </div>
+    </Layout>
+  );
+};
+export default Registration;
