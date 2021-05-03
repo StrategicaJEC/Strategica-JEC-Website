29a0f982aa98d84d111756d7b1435e52aa9bb532
RG-404 <55778353+RG-404@users.noreply.github.com>
2021-05-03 20:24:18 +0530

    add height to form

--- src/pages/registration.js
+++ src/pages/registration.js
@@ -12,7 +12,7 @@ const Registration = () => {
           src="https://docs.google.com/forms/d/e/1FAIpQLSdOQc7dwBc-2enkiD0vYaqrsjE9dsQ0VJ-A-HkbBNvyyBk9Sw/viewform?embedded=true"
           scrolling="no"
           width="640"
-          height="1369"
+          height="1600"
           frameborder="0"
           marginheight="0"
           marginwidth="0"
